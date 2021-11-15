f25fdd8ad7e3805058a14d1e5798302131280cc4
devome <evinedeng@hotmail.com>
2021-11-15 21:58:59 +0800

    nginxwebui: build images from compiled jar file

--- nginxwebui/buildx.sh
+++ nginxwebui/buildx.sh
@@ -2,10 +2,13 @@
 
 set -o pipefail
 
-repo="nevinee/nginxwebui"
-arch="linux/amd64,linux/arm64,linux/arm/v7"
+repo="${1:-nevinee}/nginxwebui"
+arch="linux/386,linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x"
 
 buildx() {
+    cd src
+    mvn clean package
+    cd ..
     docker pull tonistiigi/binfmt
     docker run --privileged --rm tonistiigi/binfmt --install all
     docker buildx create --name builder --use 2>/dev/null || docker buildx use builder
@@ -23,11 +26,16 @@ buildx() {
 git -C src pull
 ver=$(cat src/pom.xml | grep -A1 nginxWebUI | grep version | perl -pe "s|.*((\d+\.?){3,}).*|\1|")
 if [[ $ver != $(cat version 2>/dev/null) ]]; then
+    echo "构建镜像：$repo"
+    echo "构建平台：$arch"
+    echo "构建版本：$ver"
+    echo "3秒后开始编译jar并构建镜像..."
+    sleep 3
     [[ ! -d logs ]] && mkdir logs
     buildx 2>&1 | ts "[%Y-%m-%d %H:%M:%.S]" | tee -a logs/${ver}.log
     [[ $? -eq 0 ]] && {
         echo $ver > version
-        docker pushrm -s "可视化配置nginx，减小体积，支持amd64/arm64/armv7" $repo  # https://github.com/christian-korneck/docker-pushrm
+        docker pushrm -s "可视化配置nginx，减小体积，支持amd64/arm64/armv7等" $repo  # https://github.com/christian-korneck/docker-pushrm
     }
 else
     echo "当前已经是最新版本：$ver"
