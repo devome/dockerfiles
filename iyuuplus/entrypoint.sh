f901be37c0f43e393db65c2b16c42d2b04382499
devome <evinedeng@hotmail.com>
2022-01-18 18:30:45 +0800

    iyuuplus: allow change user to run iyuuplus

--- iyuuplus/entrypoint.sh
+++ iyuuplus/entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd /IYUU
 
+## 更新
 if [[ ! -d .git ]]; then
     git clone https://github.com/ledccn/IYUUPlus.git /tmp/IYUU
     #git clone https://gitee.com/ledc/iyuuplus.git /tmp/IYUU
@@ -21,18 +22,25 @@ if [ ! -d db ]; then
     mkdir db
 fi
 
+## 生成crontab
 if [[ -z "${CRON_UPDATE}" ]]; then
     minute=$(($RANDOM % 60))
     hour_start=$(($RANDOM % 6))
     hour_interval=$(($RANDOM % 4 + 6))
     CRON_UPDATE="${minute} ${hour_start}-23/${hour_interval} * * *"
 fi
+crontmp=$(mktemp)
+echo  "${CRON_UPDATE} cd /IYUU && git fetch --all && git reset --hard origin/master && git pull && php start.php restart -d" > $crontmp
+echo "设置crontab如下："
+cat $crontmp
 
-echo "设置cron..."
-echo "${CRON_UPDATE} cd /IYUU && git fetch --all && git reset --hard origin/master && git pull && php start.php restart -d" | crontab -
+## 重置权限
+groupmod -o -g "${PGID}" iyuu
+usermod -o -u "${PUID}" iyuu
+chown -R ${PUID}:${PGID} $crontmp /IYUU
+su-exec ${PUID}:${PGID} crontab $crontmp
+rm $crontmp
 
-echo "当前crontab如下："
-crontab -l
-
-php start.php start -d
+## 启动
+su-exec ${PUID}:${PGID} php start.php start -d
 exec crond -f
--- iyuuplus/readme.md
+++ iyuuplus/readme.md
@@ -2,7 +2,43 @@
 
 https://github.com/ledccn/IYUUPlus ，官方提供了Docker镜像的，建议选择官方的镜像。
 
-本镜像测试通过以后就把代码PR到了官方仓库中。
+本镜像测试通过以后就把代码PR到了官方仓库中。**但2022年1月18日，本镜像增加了一项功能：允许用户以非root用户权限运行iyuuplus，修改PUID，PGID两个环境变量即可。未推送到官方。**
+
+## 创建
+
+### docker compose
+```
+version: "3.8"
+services:
+  iyuuplus:
+    image: nevinee/iyuuplus
+    container_name: iyuuplus
+    restart: always
+    network_mode: bridge
+    hostname: iyuuplus
+    volumes:
+      - ./IYUU:/IYUU
+      - /qb种子路径/:/BT_backup
+      - /tr种子路径/:/torrent
+    environment:
+      - PUID=1000  # 以什么用记运行iyuuplus，该用户的uid
+      - PGID=100   # 以什么用记运行iyuuplus，该用户的gid
+```
+
+### docker cli
+```
+docker run -d \
+  --name iyuuplus \
+  --hostname iyuuplus \
+  --restart always \
+  --network bridge \
+  --volume $(pwd)/IYUU:/IYUU \
+  --volume /qb种子路径/:/BT_backup \
+  --volume /tr种子路径/:/torrent \
+  --env PUID=1000 `# 以什么用记运行iyuuplus，该用户的uid` \
+  --env PGID=100  `# 以什么用记运行iyuuplus，该用户的gid` \
+  nevinee/iyuuplus
+```
 
 ## 关于armv7设备的补充说明
 
