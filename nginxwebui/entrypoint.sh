4dc21778e684f4cc82cbe21ec2b2871c49b52c4d
devome <evinedeng@hotmail.com>
2022-10-21 13:47:51 +0800

    nginxwebui: run nginx and java as non-root user.

--- nginxwebui/entrypoint.sh
+++ nginxwebui/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+groupmod -o -g "${PGID}" nginx
+usermod -o -u "${PUID}" nginx
+chown -R ${PUID}:${PGID} /home/nginxWebUI.jar /home/nginxWebUI /var/lib/nginx /var/log/nginx /run/nginx
+
 cd /home
-exec java -jar -Dfile.encoding=UTF-8 -Xmx64m nginxWebUI.jar ${BOOT_OPTIONS} >/dev/null
+exec su-exec ${PUID}:${PGID} java -jar -Dfile.encoding=UTF-8 -Xmx64m nginxWebUI.jar ${BOOT_OPTIONS} >/dev/null
